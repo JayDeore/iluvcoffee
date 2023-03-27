e1711d99c99e27b7e65755001f769caa760d0c39
Jayesh Deore <jayeshvdeore25@gmail.com>
2023-03-27 13:47:39 +0530

    Added Config Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+    }),
     CoffeesModule,
     MongooseModule.forRoot('mongodb://localhost:27017/coffee-database'),
     CoffeeRatingModule,
