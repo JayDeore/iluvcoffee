d72d86998b8b215204cf3ee7dce4aeb1ae7f2f01
Jayesh Deore <jayeshvdeore25@gmail.com>
2023-03-28 19:51:12 +0530

    Added swagger module for api documentation.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,15 +8,22 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import {
+  ApiForbiddenResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger/dist/decorators';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
@@ -41,4 +48,7 @@ export class CoffeesController {
   remove(@Param('id') id: string) {
     return this.coffeesService.remove(id);
   }
+}
+function Public() {
+  throw new Error('Function not implemented.');
 }
--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,9 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger/dist';
 import { IsString } from 'class-validator';
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of a coffee' })
   @IsString()
   readonly name: string;
+  @ApiProperty({ description: 'The brand of a coffee' })
   @IsString()
   readonly brand: string;
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
